0a68239c761986e8f5ac1b70a86766d7fd1dfa97
Saishree Prakash <saishreebharadwaj@gmail.com>
2017-10-02 11:42:37 -0700

    Added one more test

--- src/testfiles/hellworld_test.go
+++ src/testfiles/hellworld_test.go
@@ -5,6 +5,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestIntCompare(t *testing.T) {
+	var num1 int = 45
+	var num2 int = 54
+	assert.Equal(t, num1, num2, "The numbers are different")
+}
+
 func TestHelloWorld(t *testing.T) {
 	var st string = "Hello World"
 	assert.Equal(t, st, "Hello World", "The two string should be the same")
